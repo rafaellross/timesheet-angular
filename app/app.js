d1296985813f6f6f7a13bb415ba3529d5ca48c55
Rafael Ross <rafaellross@gmail.com>
2017-10-12 15:25:34 +1100

    Working on select options for end of each day

--- app/app.js
+++ app/app.js
@@ -55,8 +55,9 @@ app.controller('SelectEmployeeController', ['$scope', '$http', 'EmployeesService
  
 }]);
 
-app.controller('NewTimeSheetController', ['$scope', 'EmployeesService', function($scope, EmployeesService) {    
+app.controller('NewTimeSheetController', ['$scope', 'EmployeesService', 'UtilitiesService', function($scope, EmployeesService, UtilitiesService) {    
     $scope.selecteds = EmployeesService.List;
+    $scope.WeekDays = UtilitiesService.WeekDays;
 }]);
 
 
@@ -82,8 +83,7 @@ app.service('EmployeesService', function() {
         }
     }
         return false;
-    };
-    
+    };    
     this.findObjIndex = function(arr, property, value){
         var result = -1;
         
@@ -94,5 +94,98 @@ app.service('EmployeesService', function() {
         });
         return result;
     };       
+});
+
+app.service('UtilitiesService', function(){
+   this.addMinutes = function(time, minsToAdd) {
+        function D(J){ 
+            return (J<10? '0':'') + J;
+        };
+        var piece = time.split(':');
+        var mins = piece[0]*60 + +piece[1] + +minsToAdd;
+        return D(mins%(24*60)/60 | 0) + ':' + D(mins%60);  
+    };
+    
+    this.calculateHours = function(startHour, endHour) {
+           function D(J){ 
+               return (J<10? '0':'') + J;
+           };
+           var startPiece = startHour.split(':');
+           var startMins = startPiece[0]*60 + +startPiece[1];
+
+           var endPiece = endHour.split(':');
+           var endMins = endPiece[0]*60 + +endPiece[1];
+
+           var totalMins = endMins - startMins;
+
+           return D(totalMins%(24*60)/60 | 0) + ':' + D(totalMins%60);  
+      };
+
+    this.hourToMinutes = function(hour){
+        var piece = hour.split(':');
+         var mins = piece[0]*60 + +piece[1];
+         return mins;
+    };
+
+    this.minutesToHour = function(minutes){
+         function D(J){ 
+             return (J<10? '0':'') + J;
+         };
+         return D(minutes/60 | 0) + ':' + D(minutes%60);  
+    };
+    
+    this.SelectHours = function(){
+            var optionsHour = [];
+            for (var hour = 0; hour <= (24*60)-15; hour += 15) {        
+                optionsHour.push(this.addMinutes('00:00', hour));               
+            }                                          
+            return optionsHour;            
+    };
+    
+    this.WeekDays = [
+        {
+           "Number" : 0,
+           "Description" : "Sunday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        },
+        {
+           "Number" : 1,
+           "Description" : "Monday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        },
+        {
+           "Number" : 2,
+           "Description" : "Tuesday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        },
+        {
+           "Number" : 3,
+           "Description" : "Wednesday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        },
+        {
+           "Number" : 4,
+           "Description" : "Thursday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        },
+        {
+           "Number" : 5,
+           "Description" : "Friday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        },
+        {
+           "Number" : 6,
+           "Description" : "Saturday",
+           "OptionHoursStart" : this.SelectHours(),
+           "OptionHoursEnd" : []
+        }
+    ];
+    
 });
 
